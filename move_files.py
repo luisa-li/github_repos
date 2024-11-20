1844f165698ebda50acc9fe12d362b6f4861f45b
Luisa <103768169+luisa-li@users.noreply.github.com>
2024-11-19 23:26:22 -0500

    fix

--- move_files.py
+++ move_files.py
@@ -8,14 +8,13 @@ def move_files(csv_path, target_dir, column):
     os.makedirs(target_dir, exists_ok=True)
     
     for file in df[column]:
+        breakpoint()
         target = os.path.join(target_dir, Path(file).stem)
         shutil.move(file, target)
 
 if __name__ == "__main__":
     
-    verb_samples = pd.read_csv('samples/verb_samples')
-    ct_ratio_samples = pd.read_csv('samples/ct_ratio_samples.csv')
-    wcc_size_samples = pd.read_csv('samples/wcc_size_samples.csv')
-    move_files(verb_samples, "samples/verb/", "full_path")
-    move_files(ct_ratio_samples, "samples/ct_ratio/", "full_path") 
-    move_files(wcc_size_samples, "samples/wcc_size/", "full_path")
+    move_files('samples/verb_samples.csv', "samples/verb/", "full_path")
+    move_files('samples/ct_ratio_samples.csv', "samples/ct_ratio/", "full_path") 
+    move_files('samples/wcc_size_samples.csv', "samples/wcc_size/", "full_path")
+
